1804960a53a4fdade1c61b7ca532f150e65ceb79
ruland39 <hcyrf1@nottingham.edu.my>
2024-04-20 00:32:50 +0800

    bismillah final commit - added validation

--- app/src/main/java/com/housepricepredictionapp/HouseSelectionBuyActivity.kt
+++ app/src/main/java/com/housepricepredictionapp/HouseSelectionBuyActivity.kt
@@ -44,7 +44,6 @@ class HouseSelectionBuyActivity : AppCompatActivity() {
         val numberOfBedrooms = resources.getStringArray(R.array.number_of_bedroom_array)
         val numberOfBathrooms = resources.getStringArray(R.array.number_of_bathroom_array)
         val parkingLots = resources.getStringArray(R.array.parking_lot_array)
-        val floorRanges = resources.getStringArray(R.array.floor_range_array)
         val ageOfUnits = resources.getStringArray(R.array.age_of_unit_array)
         val tenureTypes = resources.getStringArray(R.array.tenure_type_array)
         val landTitles = resources.getStringArray(R.array.land_title_array)
@@ -62,8 +61,6 @@ class HouseSelectionBuyActivity : AppCompatActivity() {
             ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberOfBathrooms)
         val parkingLotAdapter =
             ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, parkingLots)
-        val floorRangeAdapter =
-            ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, floorRanges)
         val ageOfUnitAdapter =
             ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, ageOfUnits)
         val tenureTypeAdapter =
@@ -85,113 +82,135 @@ class HouseSelectionBuyActivity : AppCompatActivity() {
         val proceedButton = binding.proceedButton
         val progressbar = binding.progressBarContainer
         progressbar.visibility = View.INVISIBLE
-
-        // TODO: Proceed button enable only when all fields are filled
         proceedButton.isEnabled = true
 
-// Proceed to result
+        // Proceed to result
         proceedButton.setOnClickListener {
-            progressbar.visibility = View.VISIBLE
 
-            // Get the selected values from the UI components
-            val selectedLocationArea = locationArea.editText?.text.toString()
-            val selectedPropertyType = propertyType.editText?.text.toString()
-            val selectedPropertySize = propertySize.editText?.text.toString()
-            val selectedNumberOfBedroom = numberOfBedroom.editText?.text.toString()
-            val selectedNumberOfBathroom = numberOfBathroom.editText?.text.toString()
-            val selectedParkingLot = parkingLot.editText?.text.toString()
-            val selectedAgeOfUnit = ageOfUnit.editText?.text.toString()
-            val selectedTenureType = tenureType.editText?.text.toString()
-            val selectedLandTitle = landTitle.editText?.text.toString()
-
-            // Convert selected values to their corresponding numeric representations
-            val newDataPoint = mutableListOf<Int>(
-                DataMapping.DataPointConverter.convertState(selectedLocationArea),
-                DataMapping.DataPointConverter.convertPropertyType(selectedPropertyType),
-                DataMapping.DataPointConverter.convertPropertySize(selectedPropertySize),
-                selectedNumberOfBedroom.toInt(),
-                selectedNumberOfBathroom.toInt(),
-                selectedParkingLot.toInt(),
-                DataMapping.DataPointConverter.convertAgeOfUnit(selectedAgeOfUnit),
-                DataMapping.DataPointConverter.convertTenureType(selectedTenureType),
-                DataMapping.DataPointConverter.convertLandTitle(selectedLandTitle)
+            // Check if all fields are filled
+            val isFieldsFilled = isFieldsFilled(
+                locationArea,
+                propertyType,
+                propertySize,
+                numberOfBedroom,
+                numberOfBathroom,
+                parkingLot,
+                ageOfUnit,
+                tenureType,
+                landTitle
             )
 
-            // Get the selected chips from facilitiesChipGroup
-            val selectedFacilitiesChips = facilitiesChipGroup.checkedChipIds
-                .map { findViewById<Chip>(it).text.toString() }
+            if(isFieldsFilled){
+                progressbar.visibility = View.VISIBLE
 
-            // Get the selected chips from nearbyFacilitiesChipGroup
-            val selectedNearbyFacilitiesChips = nearbyFacilitiesChipGroup.checkedChipIds
-                .map { findViewById<Chip>(it).text.toString() }
+                // Get the selected values from the UI components
+                val selectedLocationArea = locationArea.editText?.text.toString()
+                val selectedPropertyType = propertyType.editText?.text.toString()
+                val selectedPropertySize = propertySize.editText?.text.toString()
+                val selectedNumberOfBedroom = numberOfBedroom.editText?.text.toString()
+                val selectedNumberOfBathroom = numberOfBathroom.editText?.text.toString()
+                val selectedParkingLot = parkingLot.editText?.text.toString()
+                val selectedAgeOfUnit = ageOfUnit.editText?.text.toString()
+                val selectedTenureType = tenureType.editText?.text.toString()
+                val selectedLandTitle = landTitle.editText?.text.toString()
 
-            // Include facilities and nearby facilities in newDataPoint
-            val facilities = listOf(
-                "\uD83C\uDFCB\uFE0F Gymnasium",
-                "\uD83C\uDFCA Swimming Pool",
-                "\uD83C\uDFC3\u200D♂\uFE0F Jogging Track",
-                "\uD83D\uDEDD Playground",
-                "\uD83D\uDED2 Minimart",
-                "\uD83C\uDFDF\uFE0F Multipurpose Hall",
-                "\uD83C\uDF0A Club house"
-            )
+                // Convert selected values to their corresponding numeric representations
+                val newDataPoint = mutableListOf(
+                    DataMapping.DataPointConverter.convertState(selectedLocationArea),
+                    DataMapping.DataPointConverter.convertPropertyType(selectedPropertyType),
+                    DataMapping.DataPointConverter.convertPropertySize(selectedPropertySize),
+                    selectedNumberOfBedroom.toInt(),
+                    selectedNumberOfBathroom.toInt(),
+                    selectedParkingLot.toInt(),
+                    DataMapping.DataPointConverter.convertAgeOfUnit(selectedAgeOfUnit),
+                    DataMapping.DataPointConverter.convertTenureType(selectedTenureType),
+                    DataMapping.DataPointConverter.convertLandTitle(selectedLandTitle)
+                )
 
-            val nearbyFacilities = listOf(
-                "\uD83D\uDE86 Railway Station",
-                "\uD83D\uDE8D Bus Stop",
-                "\uD83D\uDE98 Highway",
-                "\uD83D\uDECD\uFE0F Mall",
-                "\uD83C\uDFEB School",
-                "\uD83C\uDFE5 Hospital",
-                "\uD83C\uDFDE\uFE0F Park"
-            )
+                // Get the selected chips from facilitiesChipGroup
+                val selectedFacilitiesChips = facilitiesChipGroup.checkedChipIds
+                    .map { findViewById<Chip>(it).text.toString() }
 
-            // Add values for facilities
-            for (facility in facilities) {
-                newDataPoint.add(if (facility in selectedFacilitiesChips) 1 else 0)
+                // Get the selected chips from nearbyFacilitiesChipGroup
+                val selectedNearbyFacilitiesChips = nearbyFacilitiesChipGroup.checkedChipIds
+                    .map { findViewById<Chip>(it).text.toString() }
+
+                // Include facilities and nearby facilities in newDataPoint
+                val facilities = listOf(
+                    "\uD83C\uDFCB\uFE0F Gymnasium",
+                    "\uD83C\uDFCA Swimming Pool",
+                    "\uD83C\uDFC3\u200D♂\uFE0F Jogging Track",
+                    "\uD83D\uDEDD Playground",
+                    "\uD83D\uDED2 Minimart",
+                    "\uD83C\uDFDF\uFE0F Multipurpose Hall",
+                    "\uD83C\uDF0A Club house"
+                )
+
+                val nearbyFacilities = listOf(
+                    "\uD83D\uDE86 Railway Station",
+                    "\uD83D\uDE8D Bus Stop",
+                    "\uD83D\uDE98 Highway",
+                    "\uD83D\uDECD\uFE0F Mall",
+                    "\uD83C\uDFEB School",
+                    "\uD83C\uDFE5 Hospital",
+                    "\uD83C\uDFDE\uFE0F Park"
+                )
+
+                // Add values for facilities
+                for (facility in facilities) {
+                    newDataPoint.add(if (facility in selectedFacilitiesChips) 1 else 0)
+                }
+
+                // Add values for nearby facilities
+                for (facility in nearbyFacilities) {
+                    newDataPoint.add(if (facility in selectedNearbyFacilitiesChips) 1 else 0)
+                }
+
+                // Print or use the new data point as needed
+                println("new_data_point = [$newDataPoint]")
+                Log.d("new_data_point = [$newDataPoint]", newDataPoint.toString())
+
+                // Make prediction using the model
+                val inputData = newDataPoint.map { it.toFloat() }.toFloatArray()
+                val predictedPrice = predictor.predict(inputData)
+                val predictedPriceString = predictedPrice.toInt().toString()
+
+                // ProgressBar Delay
+                Handler(Looper.getMainLooper()).postDelayed({
+                    progressbar.visibility = View.INVISIBLE
+                }, 3000)
+
+                // Create an Intent
+                val intent = Intent(this, Result::class.java)
+
+                // Put the selected values as extras in the Intent
+                intent.putStringArrayListExtra("NEW_DATA_POINT", ArrayList(newDataPoint.map { it.toString() }))
+                intent.putExtra("LOCATION_AREA", selectedLocationArea)
+                intent.putExtra("PROPERTY_TYPE", selectedPropertyType)
+                intent.putExtra("PROPERTY_SIZE", selectedPropertySize)
+                intent.putExtra("NUMBER_OF_BEDROOM", selectedNumberOfBedroom)
+                intent.putExtra("NUMBER_OF_BATHROOM", selectedNumberOfBathroom)
+                intent.putExtra("PARKING_LOT", selectedParkingLot)
+                intent.putExtra("AGE_OF_UNIT", selectedAgeOfUnit)
+                intent.putExtra("TENURE_TYPE", selectedTenureType)
+                intent.putExtra("LAND_TITLE", selectedLandTitle)
+                intent.putExtra("PREDICTED_PRICE", predictedPriceString)
+
+                // Put the selected chips as extras in the Intent
+                intent.putStringArrayListExtra("SELECTED_FACILITIES_CHIPS", ArrayList(selectedFacilitiesChips))
+                intent.putStringArrayListExtra("SELECTED_NEARBY_FACILITIES_CHIPS", ArrayList(selectedNearbyFacilitiesChips))
+
+                intent.setClass(this, Result::class.java)
+                startActivity(intent)
+
+            }
+            else{
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
             }
 
-            // Add values for nearby facilities
-            for (facility in nearbyFacilities) {
-                newDataPoint.add(if (facility in selectedNearbyFacilitiesChips) 1 else 0)
-            }
 
-            // Print or use the new data point as needed
-            println("new_data_point = [$newDataPoint]")
-            Log.d("new_data_point = [$newDataPoint]", newDataPoint.toString())
 
-            // Make prediction using the model
-            val inputData = newDataPoint.map { it.toFloat() }.toFloatArray()
-            val predictedPrice = predictor.predict(inputData)
-            val predictedPriceString = predictedPrice.toInt().toString()
 
-            // ProgressBar Delay
-            Handler(Looper.getMainLooper()).postDelayed({
-                progressbar.visibility = View.INVISIBLE
-            }, 3000)
-
-            // Create an Intent
-            val intent = Intent(this, Result::class.java)
-
-            // Put the selected values as extras in the Intent
-            intent.putStringArrayListExtra("NEW_DATA_POINT", ArrayList(newDataPoint.map { it.toString() }))
-            intent.putExtra("LOCATION_AREA", selectedLocationArea)
-            intent.putExtra("PROPERTY_TYPE", selectedPropertyType)
-            intent.putExtra("PROPERTY_SIZE", selectedPropertySize)
-            intent.putExtra("NUMBER_OF_BEDROOM", selectedNumberOfBedroom)
-            intent.putExtra("NUMBER_OF_BATHROOM", selectedNumberOfBathroom)
-            intent.putExtra("PARKING_LOT", selectedParkingLot)
-            intent.putExtra("AGE_OF_UNIT", selectedAgeOfUnit)
-            intent.putExtra("TENURE_TYPE", selectedTenureType)
-            intent.putExtra("LAND_TITLE", selectedLandTitle)
-            intent.putExtra("PREDICTED_PRICE", predictedPriceString)
-
-            // Put the selected chips as extras in the Intent
-            intent.putStringArrayListExtra("SELECTED_FACILITIES_CHIPS", ArrayList(selectedFacilitiesChips))
-            intent.putStringArrayListExtra("SELECTED_NEARBY_FACILITIES_CHIPS", ArrayList(selectedNearbyFacilitiesChips))
-
-            intent.setClass(this, Result::class.java)
-            startActivity(intent)
         }
 
     }
@@ -202,5 +221,15 @@ class HouseSelectionBuyActivity : AppCompatActivity() {
     ) {
         val autoCompleteTextView = textInputLayout.editText as? AutoCompleteTextView
         autoCompleteTextView?.setAdapter(adapter)
+    }
+
+    // Function to check if all fields are filled
+    private fun isFieldsFilled(vararg fields: TextInputLayout): Boolean {
+        for (field in fields) {
+            if (field.editText?.text.isNullOrBlank()) {
+                return false
+            }
+        }
+        return true
     }
 }
--- app/src/main/java/com/housepricepredictionapp/HouseSelectionSellActivity.kt
+++ app/src/main/java/com/housepricepredictionapp/HouseSelectionSellActivity.kt
@@ -9,6 +9,8 @@ import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.EditText
+import android.widget.Toast
 import com.google.android.material.chip.Chip
 import com.google.android.material.textfield.TextInputLayout
 import com.housepricepredictionapp.databinding.ActivityHouseSelectionSellBinding
@@ -63,121 +65,161 @@ class HouseSelectionSellActivity : AppCompatActivity() {
         val proceedButton = binding.proceedButton
         val progressbar = binding.progressBarContainer
         progressbar.visibility = View.INVISIBLE
-
-        //TODO: proceed button enable only when all fields are filled
         proceedButton.isEnabled = true
 
         //Proceed to result
         proceedButton.setOnClickListener {
 
-            progressbar.visibility = View.VISIBLE
-
-            // Get the selected values from the spinners
-            val selectedLocationArea = locationArea.editText?.text.toString()
-            val selectedPropertyType = propertyType.editText?.text.toString()
-            val selectedPropertySize = propertySize.text.toString()
-            val selectedNumberOfBedroom = numberOfBedroom.text.toString()
-            val selectedNumberOfBathroom = numberOfBathroom.text.toString()
-            val selectedParkingLot = parkingLot.text.toString()
-            val selectedAgeOfUnit = ageOfUnit.text.toString()
-            val selectedTenureType = tenureType.editText?.text.toString()
-            val selectedLandTitle = landTitle.editText?.text.toString()
-
-            // Convert selected values to their corresponding numeric representation
-            val newDataPoint = mutableListOf<Int>(
-                DataMapping.DataPointConverter.convertState(selectedLocationArea),
-                DataMapping.DataPointConverter.convertPropertyType(selectedPropertyType),
-                selectedPropertySize.toInt(),
-                selectedNumberOfBedroom.toInt(),
-                selectedNumberOfBathroom.toInt(),
-                selectedParkingLot.toInt(),
-                selectedAgeOfUnit.toInt(),
-                DataMapping.DataPointConverter.convertTenureType(selectedTenureType),
-                DataMapping.DataPointConverter.convertLandTitle(selectedLandTitle)
+            // Check if all fields are filled
+            val isFieldsFilled = isFieldsFilled(
+                locationArea,
+                propertyType,
+                tenureType,
+                landTitle
+            )
+            val isFieldsFillede = isFieldsFillede(
+                propertySize,
+                numberOfBedroom,
+                numberOfBathroom,
+                parkingLot,
+                ageOfUnit
             )
 
-            // Get the selected chips from facilitiesChipGroup
-            val selectedFacilitiesChips = facilitiesChipGroup
-                .checkedChipIds
-                .map { findViewById<Chip>(it).text.toString() }
+            if (isFieldsFilled || isFieldsFillede) {
+                progressbar.visibility = View.VISIBLE
 
-            // Get the selected chips from nearbyFacilitiesChipGroup
-            val selectedNearbyFacilitiesChips = nearbyFacilitiesChipGroup
-                .checkedChipIds
-                .map { findViewById<Chip>(it).text.toString() }
+                // Get the selected values from the spinners
+                val selectedLocationArea = locationArea.editText?.text.toString()
+                val selectedPropertyType = propertyType.editText?.text.toString()
+                val selectedPropertySize = propertySize.text.toString()
+                val selectedNumberOfBedroom = numberOfBedroom.text.toString()
+                val selectedNumberOfBathroom = numberOfBathroom.text.toString()
+                val selectedParkingLot = parkingLot.text.toString()
+                val selectedAgeOfUnit = ageOfUnit.text.toString()
+                val selectedTenureType = tenureType.editText?.text.toString()
+                val selectedLandTitle = landTitle.editText?.text.toString()
 
-            // Include facilities and nearby facilities in newDataPoint
-            val facilities = listOf(
-                "\uD83C\uDFCB\uFE0F Gymnasium",
-                "\uD83C\uDFCA Swimming Pool",
-                "\uD83C\uDFC3\u200D♂\uFE0F Jogging Track",
-                "\uD83D\uDEDD Playground",
-                "\uD83D\uDED2 Minimart",
-                "\uD83C\uDFDF\uFE0F Multipurpose Hall",
-                "\uD83C\uDF0A Club house"
-            )
+                // Convert selected values to their corresponding numeric representation
+                val newDataPoint = mutableListOf(
+                    DataMapping.DataPointConverter.convertState(selectedLocationArea),
+                    DataMapping.DataPointConverter.convertPropertyType(selectedPropertyType),
+                    selectedPropertySize.toInt(),
+                    selectedNumberOfBedroom.toInt(),
+                    selectedNumberOfBathroom.toInt(),
+                    selectedParkingLot.toInt(),
+                    selectedAgeOfUnit.toInt(),
+                    DataMapping.DataPointConverter.convertTenureType(selectedTenureType),
+                    DataMapping.DataPointConverter.convertLandTitle(selectedLandTitle)
+                )
 
-            val nearbyFacilities = listOf(
-                "\uD83D\uDE86 Railway Station",
-                "\uD83D\uDE8D Bus Stop",
-                "\uD83D\uDE98 Highway",
-                "\uD83D\uDECD\uFE0F Mall",
-                "\uD83C\uDFEB School",
-                "\uD83C\uDFE5 Hospital",
-                "\uD83C\uDFDE\uFE0F Park"
-            )
+                // Get the selected chips from facilitiesChipGroup
+                val selectedFacilitiesChips = facilitiesChipGroup
+                    .checkedChipIds
+                    .map { findViewById<Chip>(it).text.toString() }
 
-            // Add values for facilities
-            for (facility in facilities) {
-                newDataPoint.add(if (facility in selectedFacilitiesChips) 1 else 0)
+                // Get the selected chips from nearbyFacilitiesChipGroup
+                val selectedNearbyFacilitiesChips = nearbyFacilitiesChipGroup
+                    .checkedChipIds
+                    .map { findViewById<Chip>(it).text.toString() }
+
+                // Include facilities and nearby facilities in newDataPoint
+                val facilities = listOf(
+                    "\uD83C\uDFCB\uFE0F Gymnasium",
+                    "\uD83C\uDFCA Swimming Pool",
+                    "\uD83C\uDFC3\u200D♂\uFE0F Jogging Track",
+                    "\uD83D\uDEDD Playground",
+                    "\uD83D\uDED2 Minimart",
+                    "\uD83C\uDFDF\uFE0F Multipurpose Hall",
+                    "\uD83C\uDF0A Club house"
+                )
+
+                val nearbyFacilities = listOf(
+                    "\uD83D\uDE86 Railway Station",
+                    "\uD83D\uDE8D Bus Stop",
+                    "\uD83D\uDE98 Highway",
+                    "\uD83D\uDECD\uFE0F Mall",
+                    "\uD83C\uDFEB School",
+                    "\uD83C\uDFE5 Hospital",
+                    "\uD83C\uDFDE\uFE0F Park"
+                )
+
+                // Add values for facilities
+                for (facility in facilities) {
+                    newDataPoint.add(if (facility in selectedFacilitiesChips) 1 else 0)
+                }
+
+                // Add values for nearby facilities
+                for (facility in nearbyFacilities) {
+                    newDataPoint.add(if (facility in selectedNearbyFacilitiesChips) 1 else 0)
+                }
+
+                // Print or use the new data point as needed
+                println("new_data_point = [$newDataPoint]")
+                Log.d("new_data_point = [$newDataPoint]", newDataPoint.toString())
+
+                // Make prediction using the model
+                val inputData = newDataPoint.map { it.toFloat() }.toFloatArray()
+                val predictedPrice = predictor.predict(inputData)
+                val predictedPriceString = predictedPrice.toInt().toString()
+
+                //ProgressBar Delay
+                Handler(Looper.getMainLooper()).postDelayed({
+                    progressbar.visibility = View.INVISIBLE
+                }, 3000)
+
+                // Create an Intent
+                val intent = Intent(this, Result::class.java)
+
+                // Put the selected values as extras in the Intent
+                intent.putStringArrayListExtra("NEW_DATA_POINT", ArrayList(newDataPoint.map { it.toString() }))
+                intent.putExtra("LOCATION_AREA", selectedLocationArea)
+                intent.putExtra("PROPERTY_TYPE", selectedPropertyType)
+                intent.putExtra("PROPERTY_SIZE", selectedPropertySize)
+                intent.putExtra("NUMBER_OF_BEDROOM", selectedNumberOfBedroom)
+                intent.putExtra("NUMBER_OF_BATHROOM", selectedNumberOfBathroom)
+                intent.putExtra("PARKING_LOT", selectedParkingLot)
+                intent.putExtra("AGE_OF_UNIT", selectedAgeOfUnit)
+                intent.putExtra("TENURE_TYPE", selectedTenureType)
+                intent.putExtra("LAND_TITLE", selectedLandTitle)
+                intent.putExtra("PREDICTED_PRICE", predictedPriceString)
+
+                // Put the selected chips as extras in the Intent
+                intent.putStringArrayListExtra("SELECTED_FACILITIES_CHIPS", ArrayList(selectedFacilitiesChips))
+                intent.putStringArrayListExtra("SELECTED_NEARBY_FACILITIES_CHIPS", ArrayList(selectedNearbyFacilitiesChips))
+
+                intent.setClass(this, Result::class.java)
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
             }
 
-            // Add values for nearby facilities
-            for (facility in nearbyFacilities) {
-                newDataPoint.add(if (facility in selectedNearbyFacilitiesChips) 1 else 0)
-            }
 
-            // Print or use the new data point as needed
-            println("new_data_point = [$newDataPoint]")
-            Log.d("new_data_point = [$newDataPoint]", newDataPoint.toString())
-
-            // Make prediction using the model
-            val inputData = newDataPoint.map { it.toFloat() }.toFloatArray()
-            val predictedPrice = predictor.predict(inputData)
-            val predictedPriceString = predictedPrice.toInt().toString()
-
-            //ProgressBar Delay
-            Handler(Looper.getMainLooper()).postDelayed({
-                progressbar.visibility = View.INVISIBLE
-            }, 3000)
-
-            // Create an Intent
-            val intent = Intent(this, Result::class.java)
-
-            // Put the selected values as extras in the Intent
-            intent.putStringArrayListExtra("NEW_DATA_POINT", ArrayList(newDataPoint.map { it.toString() }))
-            intent.putExtra("LOCATION_AREA", selectedLocationArea)
-            intent.putExtra("PROPERTY_TYPE", selectedPropertyType)
-            intent.putExtra("PROPERTY_SIZE", selectedPropertySize)
-            intent.putExtra("NUMBER_OF_BEDROOM", selectedNumberOfBedroom)
-            intent.putExtra("NUMBER_OF_BATHROOM", selectedNumberOfBathroom)
-            intent.putExtra("PARKING_LOT", selectedParkingLot)
-            intent.putExtra("AGE_OF_UNIT", selectedAgeOfUnit)
-            intent.putExtra("TENURE_TYPE", selectedTenureType)
-            intent.putExtra("LAND_TITLE", selectedLandTitle)
-            intent.putExtra("PREDICTED_PRICE", predictedPriceString)
-
-            // Put the selected chips as extras in the Intent
-            intent.putStringArrayListExtra("SELECTED_FACILITIES_CHIPS", ArrayList(selectedFacilitiesChips))
-            intent.putStringArrayListExtra("SELECTED_NEARBY_FACILITIES_CHIPS", ArrayList(selectedNearbyFacilitiesChips))
-
-            intent.setClass(this, Result::class.java)
-            startActivity(intent)
         }
     }
 
     private fun setAdapterToAutoCompleteTextView(textInputLayout: TextInputLayout, adapter: ArrayAdapter<String>) {
         val autoCompleteTextView = textInputLayout.editText as? AutoCompleteTextView
         autoCompleteTextView?.setAdapter(adapter)
+    }
+
+    // Function to check if all fields are filled
+    private fun isFieldsFilled(vararg fields: TextInputLayout): Boolean {
+        for (field in fields) {
+            if (field.editText?.text.isNullOrBlank()) {
+                return false
+            }
+        }
+        return true
+    }
+
+    // Function to check if all fields are filled
+    private fun isFieldsFillede(vararg fields: EditText): Boolean {
+        for (field in fields) {
+            if (field.text.isNullOrBlank()) {
+                return false
+            }
+        }
+        return true
     }
 }
